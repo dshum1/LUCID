38435cf90cedde905720ee11ae5adb58a35dc6e6
Christine Li <csli@princeton.edu>
2015-12-04 00:06:50 -0500

    update to review parser

--- pythonProgs/review_parser.py
+++ pythonProgs/review_parser.py
@@ -11,20 +11,22 @@ def get_item_id(url):
 	else:
 		print "error: can't get item id"
 
-def get_reviews(item_id, num_reviews=10):
+def get_reviews(item_id, num_reviews=500):
 	p = amzn.reviews(ItemId=item_id)
 	reviews = p.full_reviews()
 	full_reviews = []
+	review_titles = []
 	counter = 0
 	for r in reviews:
 		if counter < num_reviews:
 			full_reviews.append(r.text)
+			review_titles.append(r.title)
 			counter = counter + 1
 		else:
 			break
-	return full_reviews
-	# out_file = open("reviews.json", "w")
-	# json.dump(full_reviews,out_file)
+	# return full_reviews
+	out_file = open("fuzzysocks.json", "w")
+	json.dump(review_titles+full_reviews,out_file)
 
 if __name__ == "__main__":
 	socks_url = sys.argv[1]
